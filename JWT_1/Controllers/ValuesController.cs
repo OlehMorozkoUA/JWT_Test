5dbd56b6a1b338fc62e67a2607569fd428cfa862
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-01-30 11:29:28 +0200

    Added controller for testing.

---
+++ JWT_1/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Authorize]
+        [Route("getlogin")]
+        public string GetLogin()
+            => $"Your login is {User.Identity.Name}";
+
+        [Authorize(Roles = "admin")]
+        [Route("getrole")]
+        public string GetRole()
+            => $"Your role is admin";
+    }
+}
