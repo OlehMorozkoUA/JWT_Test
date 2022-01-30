eae89cd7bec390091e03841f906ec7ad58ed3167
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-01-30 11:28:31 +0200

    Added new web api project.

---
+++ Services/classes/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Services.classes
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "Server";
+        public const string AUDIENCE = "Client";
+        const string KEY = "secretkey!";
+        public const int LIFETIME = 1;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
